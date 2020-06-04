3a644783405abbd852b3f5674349ed2800ea045c
Ify <Zasterify@gmail.com>
2020-06-04 14:54:56 -0400

    Code 3

--- repeat.py
+++ repeat.py
@@ -1,2 +1,6 @@
 def repeat(): # To define the function "repeat"
-preserve = input("Enter a word: ") # You can also type any word whatever you want to commit. 
+     preserve = input("Enter a word: ") # You can also type any word whatever you want to commit. 
+     print ("The word you entered is: ",preserve) # Remember not to forget "print" 
+
+
+
