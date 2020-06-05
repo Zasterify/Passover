ef49474b4d12b352206a03349af9c9b6f42ba303
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-06-05 12:03:04 -0400

    Added Answer to firstPractice

---
+++ Answer Keys/Laboratory 1/repeat.py
@@ -0,0 +1,11 @@
+def repeat():
+    """
+    This function accepts a user input and outputs the same wprd back to the user. 
+    """
+    userInput = input(
+        "Enter a word here: "
+    )  # This sections accepts an input from the user
+    print("The word you entered was:", userInput)  # Prints the wprd back to the user.
+
+
+repeat()  # Calls the function to run.
