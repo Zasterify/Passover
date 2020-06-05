c8ff04b20f331bd1376996946a81f0c38e8d79fb
Ify <Zasterify@gmail.com>
2020-06-05 11:58:35 -0400

    Added code 4

--- Laboratory 1/parrot.py
+++ Laboratory 1/parrot.py
@@ -1,2 +1,3 @@
 def parrot():   # To define the function 'parrot'
-    x = input("Enter a word: ")  # You can also put any word as you like
+    x = input("Enter a word: ")  # You can also put any word as you like 
+    y = int(input("Enter a number: "))  # Remember to put any number which represents how many times word to repeat
