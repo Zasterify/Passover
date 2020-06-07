e603ae33bd4d0f4e5e92dbeab8afd0d857a87f7c
Ify <Zasterify@gmail.com>
2020-06-07 10:01:40 -0400

    Updated repeat

--- Laboratory 1/repeat.py
+++ Laboratory 1/repeat.py
@@ -1,11 +1,8 @@
-def repeat(): 
-    """To define the function repeat"""
+def repeat():  # To define the function repeat
     
-    preserve = input("Enter a word: ") 
-    """You can put any word whatever you want to commit"""
+    preserve = input("Enter a word: ") # You can put any word whatever you want to commit
     
     print ("The word you entered is: ",preserve)
-"""Hint: Remember not to forget print whreever the word comes from"""
+# Hint: Remember not to forget print whreever the word comes from
 
-repeat() 
-"""Don't forget to put at the end""" 
+repeat() # Don't forget to put at the end 
