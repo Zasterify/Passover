1ff1f35f8aed8f06163902f762245fd284947e6d
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-06-06 22:29:02 -0400

    Accepted incoming change from pull

--- Laboratory 1/parrot.py
+++ Laboratory 1/parrot.py
@@ -1,28 +1,10 @@
 def parrot():   
-<<<<<<< HEAD
     '''This function is to define the parrot''' 
     
     x = input("Enter a word: ")
     y = int(input("Enter a number: "))
-=======
-    '''To define the function parrot'''
-    
-    x = input("Enter a word: ")  
-    '''You can also put any word as you like''' 
-    
-    y = int(input("Enter a number: "))  
-    '''Remember to put any number which represents how many times word to repeat'''
-    
-    print (x * y)  
-    '''Remember never to forget this'''
->>>>>>> secondPractice
 
     print ((x + " ") * y)
 
-<<<<<<< HEAD
 
 parrot()   
-=======
-parrot()  
-'''You know what i mean''' 
->>>>>>> secondPractice
