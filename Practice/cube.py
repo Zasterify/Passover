8f27f2c2fb9e35e2dbdf33db68c0b1b92b9328ba
Ify <Zasterify@gmail.com>
2020-06-09 12:36:53 -0400

    Added formula 3

--- Practice/cube.py
+++ Practice/cube.py
@@ -1,2 +1,3 @@
 length = float(input("Enter the cube edge: ")) # To enter the length in cube edge
 surface_area = length * 24 # To enter the surface area 
+print("The surface area is", surface_area, "square units") # Also to print this answer
