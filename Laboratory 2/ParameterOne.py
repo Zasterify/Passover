22775ef41b0d886030a8245352e5e59f50106c76
Ify <Zasterify@gmail.com>
2020-06-09 13:30:38 -0400

    code 2

--- Laboratory 2/ParameterOne.py
+++ Laboratory 2/ParameterOne.py
@@ -1,1 +1,3 @@
-def firstParameter(word): '''To define the fuction firstParameter and the argument is word'''
+def firstParameter(word): 
+    '''To define the fuction firstParameter and the argument is word'''
+    return word # To return the word 
