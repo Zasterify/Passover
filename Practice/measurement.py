185aa5a789dd9f60deac00225095e62c86a88c08
Ify <Zasterify@gmail.com>
2020-06-09 12:13:29 -0400

    Formula 3

--- Practice/measurement.py
+++ Practice/measurement.py
@@ -1,2 +1,3 @@
 length = float(input("Enter the length: ")) # To access any number in length  
 width = float(input("Enter the width: ")) # To access any number in width
+volume = length * width # Remember to put the equation as you decide 
