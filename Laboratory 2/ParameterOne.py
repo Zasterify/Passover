089c5d49d81b2ea51f734bbd672964808db0a7e4
Ify <Zasterify@gmail.com>
2020-06-09 13:37:03 -0400

    code 4

--- Laboratory 2/ParameterOne.py
+++ Laboratory 2/ParameterOne.py
@@ -3,3 +3,4 @@ def firstParameter(word):
     return word # To return the word 
 
 user = input('Enter a word: ') # To enter a word as you like 
+print ('The word you entered is: ',user) # To print the word from the above
