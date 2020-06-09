c50644c105141f3759caa406e7acdff2879b3d32
Ify <Zasterify@gmail.com>
2020-06-09 13:33:27 -0400

    code 3

--- Laboratory 2/ParameterOne.py
+++ Laboratory 2/ParameterOne.py
@@ -1,3 +1,5 @@
 def firstParameter(word): 
     '''To define the fuction firstParameter and the argument is word'''
     return word # To return the word 
+
+user = input('Enter a word: ') # To enter a word as you like 
