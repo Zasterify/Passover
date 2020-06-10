c6561ddf6f7440cc9e7ef8e8e95730273df791a4
Ify <Zasterify@gmail.com>
2020-06-10 01:15:33 -0400

    Temp 2

--- Laboratory 2/c2f.py
+++ Laboratory 2/c2f.py
@@ -1,3 +1,10 @@
 def c2f(): 
     '''To define the function c2f'''
+
+    celsius = int(input("Enter temperature in celsius: ")) # To enter the temperature in celsius
     
+
+
+
+
+
--- Laboratory 2/f2c.py
+++ Laboratory 2/f2c.py
@@ -1,3 +1,5 @@
 def f2c(): 
     '''To define the function f2c'''
+    fahrenheit = int(input("Enter temperature in fahrenheit: ")) # To enter the temperature in fahrenheit
     
+
