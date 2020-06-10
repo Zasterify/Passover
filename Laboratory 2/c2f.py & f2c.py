9c5039dd3c88f27279abd16ed6beec91db3c4633
Ify <Zasterify@gmail.com>
2020-06-10 01:49:38 -0400

    Added temp 3

--- Laboratory 2/c2f.py & f2c.py
+++ Laboratory 2/c2f.py & f2c.py
@@ -1,6 +1,7 @@
 def c2f(): 
     '''To define the function c2f'''
     celsius = int(input("Enter temperature in celsius: ")) # To enter the formula in celsius
+    fahrenheit = float(celsius * 9/5) + 32 # To enter the formula of fahrenheit
 
 
 
@@ -11,3 +12,4 @@ def c2f():
 def f2c(): 
     '''To define the function f2c'''
     fahrenheit = int(input("Enter temperature in fahrenheit: ")) # To enter the formula in fahrenheit
+    celsius = float(fahrenheit - 32) * 5/9 # To enter the formula of celsius
