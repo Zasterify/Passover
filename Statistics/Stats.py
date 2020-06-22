65e24cebff46a0231436023e85d4754ce35cf5b8
Ify <Zasterify@gmail.com>
2020-06-22 12:02:45 -0400

    List-added

--- Statistics/Stats.py
+++ Statistics/Stats.py
@@ -2,9 +2,5 @@ def total(listNum):
     """
     To define the function 'listNum'
     """
-    listNum = [1, 2, 3, 4, 5]
     
-    for x in listNum: # for every number (x) in list of number
-        total = total + x  # add x to the total
-        return x  # to return x  
-        
+    
