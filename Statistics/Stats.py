7f1b70397a21e46e3d4dada2f8cdda0aabe04e62
Ify <Zasterify@gmail.com>
2020-06-22 14:42:57 -0400

    Bobo

--- Statistics/Stats.py
+++ Statistics/Stats.py
@@ -1,13 +1,7 @@
-def total(listNum):  
-    """
-    To define the function 'listNum'
-    """
-    
-    total = 0 # the total of number in list is 0 
-    for n in listNum: # for every number (n) in the list of number
-        total = total + n # add n to the total
+def total(Newlist):  
 
-    return total # To return the total 
+    """ 
+    To define the function 'Newlist'
+    """
 
-samplelist = [2, 3, 4, 5] # To modify any numbers for total 
-print(total(samplelist)) # To print the total of the numbers in list
+  
