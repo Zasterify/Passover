3ecb2a46ffc69232ca3fe8feb5c036d73052d7cf
Ify <Zasterify@gmail.com>
2020-06-22 12:11:45 -0400

    list-added 4

--- Statistics/Stats.py
+++ Statistics/Stats.py
@@ -5,3 +5,4 @@ def total(listNum):
     
     total = 0 # the total of number in list is 0 
     for n in listNum: # for every number (n) in the list of number
+        total = total + n # add n to the total
