551a148c535d869b50ae438d79e01cb05cb4c076
Ify <Zasterify@gmail.com>
2020-06-23 19:29:58 -0400

    check - in

--- Statistics/Stats.py
+++ Statistics/Stats.py
@@ -71,16 +71,16 @@ def LengthSquared(givenlist):
     The function return the total which is given the numbers in givenlist 
     '''
 
-    square = 0 # This is a variable to store the total number
+    total = 0 # This is a variable to store the total number
 
     for t in givenlist: # for every number (t) in the list of numbers (givenlist)
-        total += 2  # Add t to the total 
+        total += 4  # Add t to the total 
 
-    return square  # To return the square
+    return total  # To return the square
 
 givenlist = [1, 3, 5, 7]  # i create a givenlist so that it may allow a user to pass in
 
-consideredAnswer = 8 # Also i save my considered answer so that i can use it again later
+consideredAnswer = 16 # Also i save my considered answer so that i can use it again later
 
 print("The answer i considered is: ", consideredAnswer) # To print the answer what i considered is correct
 
